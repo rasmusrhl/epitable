b405b4d16a83a89a111bf8aa43374efccc5658e4
rasmusrhl <rasmushertzumlarsen@gmail.com>
2017-10-05 19:58:04 +0200

    asdf

--- R/freq_by2.R
+++ R/freq_by2.R
@@ -1,6 +1,5 @@
 #'
 #'
-#'
 #' @description A new version of \code{freq_by} which has more features.
 #' @param dataset A dataset
 #' @param var_vector A character vector referring to column names in the
@@ -26,15 +25,24 @@ var_vector <- c("cut","color", "price" )
 freq_by <- function(dataset, var_vector, by_group = NULL, include_total = TRUE, min_cell_count = 10, htmlout = TRUE, font_css = "font-family: monospace;",
                     include_p_value = FALSE,
                     decimal_percent = 0,
-                    include_n = TRUE, include_percent = TRUE,
-                    include_cumpct = FALSE, include_cumsum = FALSE, include_subtotal = FALSE, include_n_missing = FALSE) {
+                    include_n = TRUE,
+                    include_percent = TRUE,
+                    include_cumpct = FALSE,
+                    include_cumsum = FALSE,
+                    include_subtotal = FALSE,
+                    include_n_missing = FALSE) {
+
+# Initial checks and conversion -------------------------------------------
+
 
 by_group_symbol         <- rlang::sym(by_group)
 var_vector_numeric      <- dataset[, var_vector ] %>% dplyr::select_if( is.numeric ) %>% names()
 var_vector_char         <- dataset[, var_vector ] %>% dplyr::select_if( function(x) is.factor(x) | is.character(x) ) %>% names()
 min_cell_count <- 5
 
-freq_fun <- function(dataset, var_vector_char_element) {
+
+
+freq_fun_by <- function(dataset, var_vector_char_element) {
 char_element_symbol  <-  rlang::sym(var_vector_char_element)
 by_group_vector          <-  dataset               %>% pull(UQ(by_group_symbol))
 var_vector_element_pull  <-  dataset               %>% pull(UQ(var_vector_char_element))
@@ -80,9 +88,51 @@ var_vector_element_pull  <-  dataset               %>% pull(UQ(var_vector_char_e
      count_df_piece
 }
 
+
+
+  chi_test$var_vector          <- var_vector_char_element
+
+freq_fun_total <- function(dataset, var_vector_char_element) {
+char_element_symbol  <-  rlang::sym(var_vector_char_element)
+by_group_vector          <-  dataset               %>% pull(UQ(by_group_symbol))
+var_vector_element_pull  <-  dataset               %>% pull(UQ(var_vector_char_element))
+# test
+  chi_test               <- chisq.test( x = by_group_vector, y = var_vector_element_pull  ) %>% tidy()
+  chi_test$var_vector    <- var_vector_char_element
+
+# count
+
+  dataset %>% count(UQ(by_group_symbol), UQ(char_element_symbol)) %>% complete( UQ(by_group_symbol), UQ(char_element_symbol), fill = list( n = 0 )) %>%
+
+   group_by(UQ(by_group_symbol)) %>%
+
+    mutate( "group" = UQ(by_group_symbol)[1],
+            "covariate"    = var_vector_char_element,
+            "category"     = UQ(char_element_symbol),
+            "pct"          = paste0( round( 100 * n / sum(n), decimal_percent), "%" ),
+            "cumpct"       = paste0( round( 100 * cumsum(n) / sum(n), decimal_percent), "%" ),
+            "cumsum"       = prettyNum(cumsum(n), big.mark = " " ),
+            "subtotal"     = ifelse(  dplyr::row_number(covariate)==1, yes = prettyNum( sum(n), big.mark = " "), no = NA),
+            "n_missing"    = ifelse(  dplyr::row_number(covariate)==1, yes = prettyNum( n[ rlang::are_na( category) ], big.mark = " "), no = NA),
+            "n"            = prettyNum(n, big.mark = " " )
+          ) %>% ungroup() %>%
+
+
+    transmute(     group,  covariate, category,            n,             pct,         cumpct,         cumsum,         subtotal,         n_missing )  %>%
+    # selecting the columns
+    select_if(    c(TRUE, TRUE      , TRUE    ,    include_n, include_percent, include_cumpct, include_cumsum, include_subtotal, include_n_missing ))  %>%
+
+
+# Map freq_fun_total ------------------------------------------------------
+
+
+
+
+# Map freq_fun_by ---------------------------------------------------------
+
 suppressWarnings(
 var_vector_char %>%
-  purrr::map( .f = function(x) freq_fun(dataset = dataset, var_vector_char_element = x ) ) %>%  bind_rows()  )
+  purrr::map( .f = function(x) freq_fun_by(dataset = dataset, var_vector_char_element = x ) ) %>%  bind_rows()  )
 
 }
 
